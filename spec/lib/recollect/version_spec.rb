760bab65ec48acbc8bbfe7c9afa6980d74b9c517
Thadeu Esteves Jr <tadeuu@gmail.com>
2023-02-24 19:55:26 -0300

    release v0.0.3

--- spec/lib/recollect/version_spec.rb
+++ spec/lib/recollect/version_spec.rb
@@ -4,6 +4,6 @@ require 'spec_helper'
 
 RSpec.describe Recollect do
   it 'must have a version number' do
-    expect(described_class::VERSION).to eq('0.0.2')
+    expect(described_class::VERSION).to eq('0.0.3')
   end
 end
