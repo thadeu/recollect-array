bf6be7b77e6ea150b2eb53bccfa4a82782c8c448
Thadeu Esteves Jr <tadeuu@gmail.com>
2023-02-24 21:13:00 -0300

    fix Callable modules values

--- spec/support/callable/filters.rb
+++ spec/support/callable/filters.rb
@@ -1,13 +1,19 @@
 # frozen_string_literal: true
 
 module ActiveTruthy
-  def self.call = true
+  def self.call
+    true
+  end
 end
 
 module ActiveFalsey
-  def self.call = false
+  def self.call
+    false
+  end
 end
 
 module NumbersAvailable
-  def self.call = %w[1 2]
+  def self.call
+    %w[1 2]
+  end
 end
